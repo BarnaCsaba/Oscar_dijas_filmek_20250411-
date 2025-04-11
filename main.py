72b8620d1d8f1044c352d59f28510e3da623c2ca
BarnaCsaba_PG <barna.csaba@diak.szbi-pg.hu>
2025-04-11 10:45:26 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,15 +5,41 @@ mydb = mysql.connector.connect(user='root', password='',
                                  database='oscar')
 mycursor = mydb.cursor()
 
-mycursor.execute("SELECT * FROM film")
-
-for x in mycursor:
-    print(x)
+#mycursor.execute("SELECT * FROM film")
+#
+#for x in mycursor:
+#    print(x)
 
 #2-es feladat
 mycursor.execute("""SELECT ev, cim FROM film
 WHERE nyert = 1
-ORDER BY ev ASC;""")
+ORDER BY ev ASC
+LIMIT 20""")
 
 for x in mycursor:
     print(x)
+
+#3.feladat
+mycursor.execute("""SELECT ev FROM film
+GROUP BY ev
+HAVING COUNT(ev) >= 10;""")
+
+for x in mycursor:
+    print(x)
+
+##4.feladat
+#cursor.execute("""SELECT * FROM film
+#WHERE ev >= 1939 AND ev <= 1945;""")
+#
+#for x in cursor:
+#    print(x)
+
+#5.feladat
+
+#cursor.execute("""SELECT cim FROM film
+#WHERE nyert = 1 AND bemutato > ;""")
+
+#for x in cursor:
+ #   print(x)
+
+ #6.feladat
